be40210fd6e95d168c36dd551d12c9f4d1bcfcc3
Vlad <vturshyn@gmail.com>
2018-11-04 09:27:38 +0200

    action fetch data all todos

---
+++ src/actions/fetchTodos.js
@@ -0,0 +1,49 @@
+import { FETCH_TODOS_REQUEST, 
+         FETCH_TODOS_SUCCESS, 
+         FETCH_TODOS_FAILED } from './actionTypes';
+
+
+const apiUrl = "http://localhost:3001/api";
+// Fetch all todos
+// Returns a dispatcher function
+// that dispatches an action at later time
+// than it returns a promise
+export const fetchTodos = () => {
+    return (dispatch) => {
+        dispatch(fetchTodosRequest());
+        return fetch(apiUrl)
+            .then(response => {
+                if(response.ok){
+                    response.json().then(data => {
+                        dispatch(fetchTodosSuccess(data.todos,data.message));
+                    })
+                }else{
+                    response.json().then(error => {
+                        dispatch(fetchTodosFailed(error));
+                    })
+                }
+            })
+    }
+}
+
+export const fetchTodosRequest = () => {
+    return {
+      type: FETCH_TODOS_REQUEST
+    }
+}
+  
+export const fetchTodosSuccess = (todos,message) => {
+    return {
+      type: FETCH_TODOS_SUCCESS,
+      todos: todos,
+      message: message,
+      receivedAt: Date.now
+    }
+}
+
+export const fetchTodosFailed = (error) => {
+    return {
+      type: FETCH_TODOS_FAILED,
+      error
+    }
+}
